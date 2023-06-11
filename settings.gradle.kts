c8c208ff074a72d7d368d7f8b50773661264d27f
agorafob <alexander.ptrsyn@gmail.com>
2023-06-11 23:55:33 +0400

    test version 11/06/2023

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "socket-app-2023"
+
---
+++ src/main/java/com/agorafob/SimpleServlet.java
@@ -0,0 +1,9 @@
+package com.agorafob;
+
+import java.net.Socket;
+
+public class SimpleServlet {
+    public static void main(String[] args) {
+        Socket socket = new Socket();
+    }
+}
